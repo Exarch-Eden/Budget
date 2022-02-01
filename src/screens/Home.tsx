cb5c8955a3e62c4985acc07ab52e2dd33b7172c9
Exarch-Eden <rikysmile33@gmail.com>
2022-01-31 22:44:38 -0800

    Code cleanup

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -6,32 +6,12 @@ import { selectUserData } from '../redux/reducers/userSlice';
 // import { PieChartProps } from 'react-native-chart-kit/dist/PieChart'
 import { PieChart, PieChartData } from 'react-native-svg-charts'
 
-const chartConfig = {
-    backgroundColor: '#022173',
-    backgroundGradientFrom: '#022173',
-    backgroundGradientTo: '#1b3fa0',
-    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-    // backgroundGradientFrom: "#1E2923",
-    // backgroundGradientFromOpacity: 0,
-    // backgroundGradientTo: "#08130D",
-    // backgroundGradientToOpacity: 0.5,
-    // color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-    // strokeWidth: 3, // optional, default 3
-    // barPercentage: 0.5,
-    // useShadowColorFromDataset: false // optional
-};
-
-const screenWidth = Dimensions.get('screen').width
-
 const Home = () => {
     const { income, spendings } = useAppSelector(selectUserData)
 
     useEffect(() => {
         // TODO: get user data from asyncstorage
         // and set redux user data
-
-        // console.log('screenWidth: ', screenWidth);
-        // console.log('screenWidth / 2 - 220: ', (screenWidth / 2) - 110);
     }, [])
 
     const randomColor = () => ('#' + ((Math.random() * 0xffffff) << 0).toString(16) + '000000').slice(0, 7)
@@ -56,24 +36,6 @@ const Home = () => {
                 key: `spendings`
             },
         ]
-
-        // for react-native-chart-kit
-        // return [
-        //     {
-        //         name: 'Income',
-        //         population: income || 50,
-        //         color: '#ccc',
-        //         legendFontColor: '#ccc',
-        //         legendFontSize: 15
-        //     },
-        //     {
-        //         name: 'Spendings',
-        //         population: spendings || 50,
-        //         color: 'red',
-        //         legendFontColor: 'red',
-        //         legendFontSize: 15
-        //     }
-        // ]
     }
 
     return (
@@ -91,15 +53,6 @@ const Home = () => {
                         innerRadius='75%'
                     />
                 </View>
-                {/* <PieChart
-                    data={generatePieData()}
-                    chartConfig={chartConfig}
-                    width={screenWidth - 40}
-                    height={220}
-                    accessor='population'
-                    backgroundColor='transparent'
-                    paddingLeft='0'
-                /> */}
             </View>
 
         </View>
