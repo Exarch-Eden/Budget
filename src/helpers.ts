eb1c4716c98d4c0f28e39149b11155d92eafddcd
Exarch-Eden <rikysmile33@gmail.com>
2022-02-06 23:17:17 -0800

    Created helpers TS file

---
+++ src/helpers.ts
@@ -0,0 +1,46 @@
+import { MessageOptions as MessageOptionsNative, MessageType, Position, showMessage as showMessageNative } from "react-native-flash-message"
+
+// remove required "message" prop in interface (because helper functions
+// already have it as a mandatory parameter)
+interface MessageOptions extends Omit<MessageOptionsNative, 'message'> {}
+
+type ShowMessageFunc = (
+    message: string,
+    description?: string,
+    type?: MessageType,
+    duration?: number,
+    position?: Position,
+    rest?: MessageOptions
+) => void
+
+type ShowMessageAltFunc = (
+    message: string,
+    description?: string,
+    duration?: number,
+    rest?: MessageOptions
+) => void
+
+const showMessage: ShowMessageFunc = (message, description = '', type = 'default', duration = 3000, position = 'top', rest = {}) => {
+    showMessageNative({
+        message,
+        description,
+        type,
+        duration,
+        position,
+        ...rest
+    })
+}
+
+const showDanger: ShowMessageAltFunc = (message, description, duration = 5000, rest) => {
+    showMessage(message, description, 'danger', duration, undefined, rest)
+}
+
+const showSuccess: ShowMessageAltFunc = (message, description, duration, rest) => {
+    showMessage(message, description, 'success', duration, undefined, rest)
+}
+
+export default {
+    showMessage,
+    showDanger,
+    showSuccess
+}
