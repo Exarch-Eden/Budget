460d0707fac8ee76b0b991ac8b60278ae979c374
Exarch-Eden <rikysmile33@gmail.com>
2022-02-15 20:41:39 -0800

    Updated Text component
    Added proper logic for receiving native Text props

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -5,17 +5,16 @@ import { THEME, STYLES } from '../styles'
 interface TextProps extends TextPropsNative {
     dark?: boolean,
     style?: StyleProp<TextStyle>
-    rest?: TextPropsNative,
     children?: React.ReactNode
 }
 
-const Text: React.FC<TextProps> = ({ dark, style, rest, children }) => {
-
+const Text: React.FC<TextProps> = ({ dark, style, children, ...rest }) => {
     return (
-        <TextNative style={[
-            style,
-            { color: dark ? THEME.SECONDARY.Dark : THEME.PRIMARY.Light }
-        ]}
+        <TextNative
+            style={[
+                style,
+                { color: dark ? THEME.SECONDARY.Dark : THEME.PRIMARY.Light }
+            ]}
             {...rest}
         >
             {React.Children.toArray(children)}
