7bbf40d9777fd3dcae3de8bea21a4f219f29628b
Exarch-Eden <rikysmile33@gmail.com>
2022-05-17 23:31:00 -0700

    Created types and values file for slide-related content
    Slides in Home screen

---
+++ src/constants/types/slides.ts
@@ -0,0 +1,13 @@
+import React from "react"
+
+export type InfoSlideType = 'spend' | 'all'
+
+export interface InfoSlideData {
+    type: InfoSlideType
+}
+
+interface InfoSlideRenderFuncParams {
+    item: InfoSlideData
+}
+
+export type InfoSlideRenderFunc = (params: InfoSlideRenderFuncParams) => React.ReactElement
---
+++ src/constants/values/slides.ts
@@ -0,0 +1,11 @@
+import { InfoSlideData } from "../types/slides";
+
+const INFO_SLIDES: InfoSlideData[] = [
+    {
+        type: 'spend'
+    }
+]
+
+export {
+    INFO_SLIDES
+}
