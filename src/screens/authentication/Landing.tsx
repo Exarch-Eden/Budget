ab035fc5c54280880651a55c01204ec4c33fddb8
Exarch-Eden <rikysmile33@gmail.com>
2022-06-06 21:22:59 -0700

    Created Landing screen

---
+++ src/screens/authentication/Landing.tsx
@@ -0,0 +1,19 @@
+import { View } from 'react-native'
+import React from 'react'
+import Page from '../../components/Page'
+import Text from '../../components/Text'
+import Button from '../../components/Button'
+
+/**
+ * This is the landing page.
+ */
+const Landing = () => {
+    return (
+        <Page>
+            <Text>Landing Page</Text>
+            
+        </Page>
+    )
+}
+
+export default Landing
