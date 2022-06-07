3cf3ede81f9b5cf25512cdc490e3607292772f0c
Exarch-Eden <rikysmile33@gmail.com>
2022-06-06 21:22:39 -0700

    Created Page component
    To be used by all screen components

---
+++ src/components/Page.tsx
@@ -0,0 +1,19 @@
+import { View, Text } from 'react-native'
+import React, { Children, FC, ReactNode } from 'react'
+import { STYLES } from '../styles'
+
+interface PageProps {
+    children?: ReactNode
+}
+
+const Page: FC<PageProps> = ({
+    children
+}) => {
+    return (
+        <View style={STYLES.page}>
+            {Children.toArray(children)}
+        </View>
+    )
+}
+
+export default Page
