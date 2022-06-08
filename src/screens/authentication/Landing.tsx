0a6103b7201f9c17f5ebff2b190bc010303fe7f6
Exarch-Eden <rikysmile33@gmail.com>
2022-06-07 21:35:48 -0700

    Updated Landing screen

--- src/screens/authentication/Landing.tsx
+++ src/screens/authentication/Landing.tsx
@@ -3,15 +3,19 @@ import React from 'react'
 import Page from '../../components/Page'
 import Text from '../../components/Text'
 import Button from '../../components/Button'
+import { useNavigation } from '@react-navigation/native'
 
 /**
  * This is the landing page.
  */
 const Landing = () => {
+    const navigation = useNavigation()
+
     return (
         <Page>
-            <Text>Landing Page</Text>
-            
+            <Text style={{ marginBottom: 10 }}>Landing Page</Text>
+            <Button onPress={() => navigation.navigate('Home')} label='Guest' />
+            <Button label='Login' />
         </Page>
     )
 }
