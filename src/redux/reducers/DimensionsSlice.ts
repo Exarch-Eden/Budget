1892bc350e0727e40650e3a182cd2c55aea38350
Exarch-Eden <rikysmile33@gmail.com>
2022-06-12 20:28:55 -0700

    Created dimensions reducer

---
+++ src/redux/reducers/DimensionsSlice.ts
@@ -0,0 +1,71 @@
+import { SliceCaseReducers, PayloadAction } from "@reduxjs/toolkit";
+import { Dimensions } from "../../constants/types/dimensions";
+import { createGenericSlice } from "../GenericSlice";
+import { RootState } from "../store";
+
+interface DimensionsReducer {
+    dimensions: Dimensions
+}
+
+const initialState: DimensionsReducer = {
+    dimensions: {
+        screenWidth: 0,
+        screenHeight: 0,
+        windowWidth: 0,
+        windowHeight: 0,
+    }
+}
+
+const reducers: SliceCaseReducers<DimensionsReducer> = {
+    setDimensions: (state, action: PayloadAction<Partial<Dimensions>>) => {
+        console.log('Setting dimensions redux: \n', action.payload);
+        
+        state.dimensions = {
+            ...state.dimensions,
+            ...action.payload
+        }
+    },
+    setWindowWidth: (state, action: PayloadAction<number>) => {
+        console.log('Setting window width redux: ', action.payload);
+        
+        state.dimensions.windowWidth = action.payload
+    },
+    setWindowHeight: (state, action: PayloadAction<number>) => {
+        console.log('Setting window height redux: ', action.payload);
+        
+        state.dimensions.windowHeight = action.payload
+    },
+    setScreenWidth: (state, action: PayloadAction<number>) => {
+        console.log('Setting screen width redux: ', action.payload);
+        
+        state.dimensions.screenWidth = action.payload
+    },
+    setScreenHeight: (state, action: PayloadAction<number>) => {
+        console.log('Setting screen height redux: ', action.payload);
+        
+        state.dimensions.screenHeight = action.payload
+    },
+}
+
+export const DimensionsSlice = createGenericSlice<DimensionsReducer>(
+    'dimensions',
+    initialState,
+    reducers
+)
+
+export const {
+    setDimensions,
+    setWindowWidth,
+    setWindowHeight,
+    setScreenWidth,
+    setScreenHeight
+} = DimensionsSlice.actions
+
+export const selectDimensions = (state: RootState) => state.dimensions.dimensions;
+export const selectScreenWidth = (state: RootState) => state.dimensions.dimensions.screenWidth;
+export const selectScreenHeight = (state: RootState) => state.dimensions.dimensions.screenHeight;
+export const selectWindowWidth = (state: RootState) => state.dimensions.dimensions.windowWidth;
+export const selectWindowHeight = (state: RootState) => state.dimensions.dimensions.windowHeight;
+
+
+export default DimensionsSlice.reducer
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
 import UserDataReducer from './reducers/UserSlice'
 import ActivityReducer from './reducers/ActivitySlice'
 import MonetaryReducer from './reducers/MonetarySlice'
+import DimensionsReducer from './reducers/DimensionsSlice'
 
 export const store = configureStore({
     reducer: {
         user: UserDataReducer,
         activity: ActivityReducer,
-        monetary: MonetaryReducer
+        monetary: MonetaryReducer,
+        dimensions: DimensionsReducer
     }
 })
 
