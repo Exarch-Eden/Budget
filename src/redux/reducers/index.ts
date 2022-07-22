b571bc4a2bb8360cee74c110a120cb33dcec9ac9
Exarch-Eden <rikysmile33@gmail.com>
2022-07-21 22:09:19 -0700

    Created index file for reducers

---
+++ src/redux/reducers/index.ts
@@ -0,0 +1,13 @@
+import ActivitySlice from "./ActivitySlice";
+import DimensionsSlice from "./DimensionsSlice";
+import LoadingSlice from "./LoadingSlice";
+import MonetarySlice from "./MonetarySlice";
+import UserSlice from "./UserSlice";
+
+export {
+    ActivitySlice,
+    DimensionsSlice,
+    LoadingSlice,
+    MonetarySlice,
+    UserSlice
+}
