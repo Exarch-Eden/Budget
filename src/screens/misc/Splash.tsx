6d317f1ae21b41e48ec1f1a1409d30dad978a90c
Exarch-Eden <rikysmile33@gmail.com>
2022-07-21 21:45:05 -0700

    Created Splash screen

---
+++ src/screens/misc/Splash.tsx
@@ -0,0 +1,13 @@
+import { Text } from "react-native";
+import React from "react";
+import { Page } from "../../components";
+
+const Splash: React.FC = () => {
+    return (
+        <Page>
+            <Text>Splash</Text>
+        </Page>
+    );
+};
+
+export default Splash;
