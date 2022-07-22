3d7322c864340dccb378fc20cfdf57597b40000f
Exarch-Eden <rikysmile33@gmail.com>
2022-07-21 22:25:11 -0700

    Created useLoading hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,9 @@
 import useDimensions from "./useDimensions";
 import useExpenseDimensions from "./useExpenseDimensions";
+import useLoading from "./useLoading";
 
 export {
     useDimensions,
-    useExpenseDimensions
+    useExpenseDimensions,
+    useLoading
 }
