6394a1af072973f743ea77d8b052e3c8a4d665da
Exarch-Eden <rikysmile33@gmail.com>
2022-07-21 21:45:11 -0700

    Created index file for screens

---
+++ src/screens/index.ts
@@ -0,0 +1,11 @@
+import Splash from "./misc/Splash";
+import Home from "./bottom-tabs/Home";
+import Settings from "./bottom-tabs/Settings";
+import Landing from "./authentication/Landing";
+
+export {
+    Splash,
+    Home,
+    Settings,
+    Landing
+}
