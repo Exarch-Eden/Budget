b6ffffec841ffdc0b3296f505db5a1c335262ac8
Exarch-Eden <rikysmile33@gmail.com>
2022-07-21 21:44:56 -0700

    Created index file for components

---
+++ src/components/index.ts
@@ -0,0 +1,21 @@
+import Button from "./Button";
+import GenericModal from "./GenericModal";
+import InfoCard from "./InfoCard";
+import Page from "./Page";
+import Text from "./Text";
+import MonthlyExpenseList from "./home-slides/MonthlyExpenseList";
+import MonthlyExpenseSlide from "./home-slides/MonthlyExpenseSlide";
+import AddTagModal from "./modals/AddTagModal";
+import AddValueModal from "./modals/AddValueModal";
+
+export {
+    Button,
+    GenericModal,
+    InfoCard,
+    Page,
+    Text,
+    MonthlyExpenseList,
+    MonthlyExpenseSlide,
+    AddTagModal,
+    AddValueModal
+}
