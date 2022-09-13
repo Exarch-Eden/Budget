469714a3f501f28c6bf3e20cb332dfc1df2253e7
Exarch-Eden <rikysmile33@gmail.com>
2022-09-12 23:11:27 -0700

    Moved files from src to src_old
    Also updated imports for App TSX and index JS files

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { AppRegistry } from "react-native";
 import { Provider } from "react-redux";
 import App from "./App";
 import { name as appName } from "./app.json";
-import { store } from "./src/redux/store";
+import { store } from "./src_old/redux/store";
 
 const AppProviderWrapper = () => {
     return (
