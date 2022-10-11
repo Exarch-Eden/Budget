d391f390990f8dd848ab2a9835bdb154556abea8
Exarch-Eden <rikysmile33@gmail.com>
2022-10-11 00:56:57 -0700

    Created styles index file
    Contains primary and secondary colours for the app theme

---
+++ src/styles/index.ts
@@ -0,0 +1,23 @@
+const COLORS = {
+    PRIMARY: {
+        Dark: "#1F1C1F",
+        Light: "#C6B093",
+        Red: "#EB5757",
+        Green: "#6FCF97",
+        Yellow: "#F2C94C",
+    },
+    SECONDARY: {
+        Dark: "#3A343A",
+        Red: "red",
+        // Red: '#E62929',
+        Green: "#4D8F68",
+        // Green: 'green',
+        // Green: '#49C27B',
+        Yellow: "yellow",
+        // Yellow: '#EFBB1C'
+    }
+}
+
+export {
+    COLORS
+}
