930d74ec07d246587e792b8c0fb3414008ca62b4
Exarch-Eden <rikysmile33@gmail.com>
2022-10-12 00:18:54 -0700

    Updated Home screen
    Implemented AccountCarousel component

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,10 +1,19 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import Carousel from "react-native-snap-carousel";
+import { Account } from "../../types/account.types";
+import AccountCarousel from "./components/AccountCarousel";
 
 const Home = () => {
+    const [accounts, setAccounts] = useState<Account[]>([])
+
     return (
         <View>
             <Text>Home</Text>
+            <AccountCarousel
+                data={accounts}
+                sliderWidth={}
+            />
         </View>
     );
 };
