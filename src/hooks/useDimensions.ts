13d362a3da7a41e7db1d8dc32e8bf6933107d618
Exarch-Eden <rikysmile33@gmail.com>
2022-10-25 20:47:49 -0700

    Updated useDimensions hook

--- src/hooks/useDimensions.ts
+++ src/hooks/useDimensions.ts
@@ -1,12 +1,24 @@
+import { setScreenHeight, setScreenWidth, setWindowHeight, setWindowWidth } from "../../src_old/redux/reducers/DimensionsSlice"
 import { useAppDispatch, useAppSelector } from "../redux/hooks"
-import { selectDimensions } from "../redux/reducers/dimensions"
+import { selectDimensions, setDimensions } from "../redux/reducers/dimensions"
+import { Dimensions } from "../types/misc"
 
 const useDimensions = () => {
     const dimensions = useAppSelector(selectDimensions)
 
     const dispatch = useAppDispatch()
 
-    
+    return {
+        screenWidth: dimensions.screenWidth,
+        screenHeight: dimensions.screenHeight,
+        windowWidth: dimensions.windowWidth,
+        windowHeight: dimensions.windowHeight,
+        setScreenWidth: (width: number) => dispatch(setScreenWidth(width)),
+        setScreenHeight: (height: number) => dispatch(setScreenHeight(height)),
+        setWindowWidth: (width: number) => dispatch(setWindowWidth(width)),
+        setWindowHeight: (height: number) => dispatch(setWindowHeight(height)),
+        setDimensions: (dimensions: Dimensions) => dispatch(setDimensions(dimensions))
+    }
 }
 
 export default useDimensions
