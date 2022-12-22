6f6dd29aeb571590815eb3cf9ab47258c4da2118
Exarch-Eden <56244072+Exarch-Eden@users.noreply.github.com>
2022-12-21 19:28:43 -0800

    Created FLOATING ADD BUTTON size constants

---
+++ src/constants/sizes.ts
@@ -0,0 +1,10 @@
+import { SizeRatioConstant } from "../types/misc"
+
+const FLOATING_ADD_BUTTON = 0.211
+// relative to FLOATING_ADD_BUTTON value
+const FLOATING_ADD_BUTTON_PLUS_ICON = 0.3846
+
+export {
+    FLOATING_ADD_BUTTON,
+    FLOATING_ADD_BUTTON_PLUS_ICON
+}
