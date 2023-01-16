73c59ac5d618afc5d509130a2ff8a79ddd7c9ab6
Exarch-Eden <56244072+Exarch-Eden@users.noreply.github.com>
2023-01-15 22:47:02 -0800

    Refactored usePanResponder hook
    Implemented newly-created PanResponder redux
    
    Implemented proper pan repsonder ref logic from AddButton component
    
    Auto formatted

--- src/hooks/usePanResponder.ts
+++ src/hooks/usePanResponder.ts
@@ -1,41 +1,88 @@
-import { useRef, useState } from "react"
-import { PanResponder } from "react-native"
+import { useRef, useState } from "react";
+import { PanResponder } from "react-native";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import {
+    selectIsPanning,
+    selectPanGestureX,
+    selectPanGestureY,
+    setIsPanning,
+    setPanGestureX,
+    setPanGestureY,
+} from "../redux/reducers/pan-responder";
 
 const usePanResponder = () => {
-    const [panGestureX, setPanGestureX] = useState(0)
-    const [panGestureY, setPanGestureY] = useState(0)
+    // const [panGestureX, setPanGestureX] = useState(0)
+    // const [panGestureY, setPanGestureY] = useState(0)
+    // const [isPanning, setIsPanning] = useState(false)
 
+    const panGestureX = useAppSelector(selectPanGestureX);
+    const panGestureY = useAppSelector(selectPanGestureY);
+    const isPanning = useAppSelector(selectIsPanning);
+
+    const dispatch = useAppDispatch()
+
+    // NOTE: docs below
+    // https://reactnative.dev/docs/gesture-responder-system
+    // https://reactnative.dev/docs/panresponder
+    // NOTE: below link details the issue with svgs and pan responder
+    // https://github.com/software-mansion/react-native-svg/issues/473
     const gestureRef = useRef(
         PanResponder.create({
-            // onStartShouldSetPanResponder: () => true,
-            // onStartShouldSetPanResponderCapture: () => true,
-            // onMoveShouldSetPanResponder: () => true,
-            // onMoveShouldSetPanResponderCapture: () => true,
-            onPanResponderGrant: (event) => {
-                // TESTING
-                console.log("root gesture granted: ", event.nativeEvent.identifier);
-            },
-            onPanResponderMove: (event, gestureState) => {
-                const rootX = event.nativeEvent.pageX;
-                const rootY = event.nativeEvent.pageY;
+            // Ask to be the responder:
+            onStartShouldSetPanResponder: (evt, gestureState) => true,
+            onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+            onMoveShouldSetPanResponder: (evt, gestureState) => true,
+            onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+            onPanResponderGrant: (evt, gestureState) => {
+                console.log("gesture granted: ", evt.nativeEvent.identifier);
 
-                // TESTING
-                console.log(`root gesture (x, y): (${rootX}, ${rootY})`);
-
-                // TODO: add setter logic here for the x and y
+                dispatch(setIsPanning(true))
             },
-            onPanResponderTerminate: (event, gestureState) => {
+            onPanResponderReject: () => {
                 // TESTING
-                console.log("root gesture terminated: ", event.nativeEvent.identifier);
+                console.log("gesture rejected");
+            },
+            onPanResponderMove: (evt, gestureState) => {
+                const event = evt.nativeEvent;
+                const rootX = event.pageX;
+                const rootY = event.pageY;
+
+                // console.log(`gesture (x, y): (${rootX}, ${rootY})`);
+                dispatch(setPanGestureX(rootX))
+                dispatch(setPanGestureY(rootY))
+            },
+            onPanResponderRelease: (evt, gestureState) => {
+                // The user has released all touches while this view is the
+                // responder. This typically means a gesture has succeeded
+
+                const rootX = evt.nativeEvent.pageX;
+                const rootY = evt.nativeEvent.pageY;
+
+                console.log(`gesture released at (${rootX}, ${rootY})`);
+
+                dispatch(setIsPanning(false))
+            },
+            onPanResponderTerminate: (evt, gestureState) => {
+                // Another component has become the responder, so this gesture
+                // should be cancelled
+
+                console.log("gesture terminated: ", evt.nativeEvent.identifier);
             },
             onPanResponderTerminationRequest: () => true,
-            onShouldBlockNativeResponder: () => false
+            onShouldBlockNativeResponder: (evt, gestureState) => {
+                // Returns whether this component should block native components from becoming the JS
+                // responder. Returns true by default. Is currently only supported on android.
+                return false;
+            },
         })
-    ).current
+    ).current;
 
     return {
-        gestureRef
-    }
-}
+        gestureRef,
+        panGestureX,
+        panGestureY,
+        isPanning
+    };
+};
 
-export default usePanResponder
+export default usePanResponder;
