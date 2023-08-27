6569717a24757b7cf63dd974b4df10d6bf4ac8a7
Exarch-Eden <rikysmile33@gmail.com>
2023-08-26 21:38:35 -0700

    button on press counter demo

--- app/src/main/java/com/example/budget/SecondFragment.java
+++ app/src/main/java/com/example/budget/SecondFragment.java
@@ -1,9 +1,11 @@
 package com.example.budget;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,6 +16,8 @@ import com.example.budget.databinding.FragmentSecondBinding;
 public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
+//    private TextView myTextView = ((TextView) findViewById(R.id.textview_second));
+    private int counter = 0;
 
     @Override
     public View onCreateView(
@@ -32,8 +36,16 @@ public class SecondFragment extends Fragment {
         binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+//                NavHostFragment.findNavController(SecondFragment.this)
+//                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                counter++;
+                Log.i("counter", (new Integer(counter)).toString());
+                Log.i("view ", view.toString());
+                Log.i("r.id", (new Integer(R.id.textview_second)).toString());
+//                Log.i("found view", (view.getParent());
+//                TextView myTextView = (TextView) findViewById(R.id.textview_second);
+                TextView myTextView = binding.textviewSecond;
+                myTextView.setText("Clicked " + counter + " times");
             }
         });
     }
